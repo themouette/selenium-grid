b004aa12f8c0da3e7752ac2353bb21d3d97d1d30
Julien Muetton <jmuetton.ext@almerys.com>
2013-07-10 16:37:53 +0200

    Add the grid parsing ability.

---
+++ tests/statusTest.js
@@ -0,0 +1,28 @@
+var chai = require('chai');
+var fs = require('fs');
+var status = require('../src/status');
+var assert = chai.assert;
+
+describe("parse grid status HTML", function () {
+
+    it("Should be possible to parse grid HTML", function (done) {
+        var html = fs.readFileSync(__dirname + '/../fixtures/html/console.html');
+        status.parseHtml(html, function (err, res) {
+            assert.deepEqual(res, [{
+                "platform": "XP",
+                "seleniumProtocol": "WebDriver",
+                "browserName": "chrome",
+                "maxInstances": "2",
+                "version": "latest"
+            }, {
+                "platform": "XP",
+                "seleniumProtocol": "WebDriver",
+                "browserName": "firefox",
+                "maxInstances": "2",
+                "version": "latest"
+            }]);
+            done();
+        });
+    });
+
+});

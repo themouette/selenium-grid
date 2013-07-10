bc3133194cc0f365cb735c3a9a85af176c91c526
Julien Muetton <jmuetton.ext@almerys.com>
2013-07-10 18:12:15 +0200

    directory layout.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    runner: require('./runner'),
+    testCase: require('./testCase')
+};
---
+++ src/testCase.js
@@ -0,0 +1,13 @@
+module.exports = function testCase() {
+};
+
+testCase.prototype.before = function (err, desired) {
+};
+
+testCase.prototype.run = function (remote, desired, done) {
+    done();
+};
+
+testCase.prototype.after = function (err, desired) {
+};
+

08659bdc8ee07913bad1a4ada6899c3b7c2e612e
Julien Muetton <jmuetton.ext@almerys.com>
2013-07-11 11:45:45 +0200

    TestCase can be extended.

---
+++ tests/testCaseTest.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var TestCase = require('../src/testCase');
+var assert = chai.assert;
+
+describe('testCase', function () {
+    it('should be possible to extend', function(done) {
+        var Case2 = TestCase.extend({
+            foo: function () {
+                done();
+            }
+        });
+
+        var t = new Case2();
+        assert.instanceOf(t, TestCase);
+        t.foo();
+    });
+});

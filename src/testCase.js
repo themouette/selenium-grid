8676bd7bf856ab2f58182d0cc83784185b098ad7
Julien Muetton <jmuetton.ext@almerys.com>
2013-07-11 11:45:45 +0200

    TestCase can be extended.

--- src/testCase.js
+++ src/testCase.js
@@ -1,5 +1,9 @@
-module.exports = function testCase() {
-};
+var _ = require('lodash');
+
+module.exports = testCase;
+
+function testCase() {
+}
 
 testCase.prototype.before = function (err, desired) {
 };
@@ -11,3 +15,35 @@ testCase.prototype.run = function (remote, desired, done) {
 testCase.prototype.after = function (err, desired) {
 };
 
+testCase.extend = function(protoProps, staticProps) {
+    var parent = this;
+    var child;
+
+    // The constructor function for the new subclass is either defined by you
+    // (the "constructor" property in your `extend` definition), or defaulted
+    // by us to simply call the parent's constructor.
+    if (protoProps && _.has(protoProps, 'constructor')) {
+      child = protoProps.constructor;
+    } else {
+      child = function(){ return parent.apply(this, arguments); };
+    }
+
+    // Add static properties to the constructor function, if supplied.
+    _.extend(child, parent, staticProps);
+
+    // Set the prototype chain to inherit from `parent`, without calling
+    // `parent`'s constructor function.
+    var Surrogate = function(){ this.constructor = child; };
+    Surrogate.prototype = parent.prototype;
+    child.prototype = new Surrogate;
+
+    // Add prototype properties (instance properties) to the subclass,
+    // if supplied.
+    if (protoProps) _.extend(child.prototype, protoProps);
+
+    // Set a convenience property in case the parent's prototype is needed
+    // later.
+    child.__super__ = parent.prototype;
+
+    return child;
+};

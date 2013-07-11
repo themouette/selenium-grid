fd4538b7928e8a3780f9be76b9237f4ff54e771d
Julien Muetton <themouette@gmail.com>
2013-07-11 00:16:44 +0000

    Tests for runner.

--- src/runner.js
+++ src/runner.js
@@ -1,22 +1,26 @@
 var _ = require('lodash');
-var status = require('status');
+var async = require('async');
+var status = require('./status');
 
-module.exports  = function run(configuration, tests) {
-    var startegyDone = function (err) {},
-        testDone = function (err, desired, test) {},
-        browserDone = function (err, desired) {};
+module.exports  = function run(config, tests) {
+    var startegyDone = config.after || function (err) {},
+        testDone = config.afterEach || function (err, desired, test) {},
+        browserDone = config.afterBrowser || function (err, desired) {};
     // ensure configuration meets requirements
-    configuration = _.merge({}, require("configuration.json"), configuration);
+    config = _.merge({}, require("./config.json"), config);
 
     // check wich browsers are available
-    checkBrowsersCapabilities(configuration, runTestsForBrowsers);
+    checkBrowsersCapabilities(config, runTestsForBrowsers);
 
     function runTestsForBrowsers(err, browsers) {
         if (err) {
             return startegyDone(err);
         }
 
-        async.forEach(browsers, _.partial(runTestsForBrowser, configuration, tests, testDone, browserDone), startegyDone);
+        var callback = _.partial(runTestsForBrowser,
+            config, tests, testDone, browserDone);
+
+        async.each(browsers, callback, startegyDone);
     }
 };
 
@@ -24,40 +28,51 @@ function checkBrowsersCapabilities(configuration, callback) {
     var browsers = configuration.browsers;
 
     if (configuration.skipCapabilitiesCheck) {
-        return callback(browsers);
+        // invoke with the full browser stack
+        return callback(null, browsers);
     }
 
-    status.available(configuration.remoteCfg, function (err, availableBrowsers) {
-        if (err !== null) {
-            return callback(new Error('Could not connect to selenium grid, did you started it?'));
-        }
+    status.available(
+        configuration.remoteCfg,
+        function (err, availableBrowsers) {
+            if (err !== null) {
+                return callback(new Error([
+                    'Could not connect to selenium grid, ',
+                    'did you started it?'
+                ].join('')));
+            }
 
-        var foundBrowsers = browsers.filter(function(desired) {
-            return availableBrowsers.some(function(available) {
-                return available.browserName === desired.browserName &&
-                    available.version === desired.version;
+            var foundBrowsers = browsers.filter(function(desired) {
+                return availableBrowsers.some(function(available) {
+                    return available.browserName === desired.browserName && (
+                            !desired.version ||
+                            available.version === desired.version);
+                });
             });
-        });
 
-        if (foundBrowsers.length === 0) {
-            return callback(new Error('No browsers found in the grid, did you started your VMs?'));
-        }
+            if (foundBrowsers.length === 0) {
+                return callback(new Error([
+                    'No matching browsers found in the grid, ',
+                    'did you started your VMs?'
+                ].join('')));
+            }
 
-        callback(err, foundBrowsers);
+            callback(err, foundBrowsers);
     });
 }
 
 
-function runTestsForBrowser(configuration, tests, testDone, browserDone, desired) {
-    var queue = async.queue(runTest, opt.concurrency);
+function runTestsForBrowser(config, tests, testDone, browserDone, desired, doneCb) {
+    var queue = async.queue(runTest, config.concurrency);
     // when all tests are done for browser
-    queue.drain = function () {
-        browserDone(null, desired);
+    queue.drain = function (err) {
+        browserDone(err, desired);
+        doneCb();
     };
 
     tests.forEach(function (test) {
         var task = {
-            remoteCfg: configuration.remoteCfg,
+            remoteCfg: config.remoteCfg,
             desired: desired,
             test: test
         };
@@ -71,7 +86,7 @@ function runTestsForBrowser(configuration, tests, testDone, browserDone, desired
 }
 
 function runTest(task, doneCb) {
-    var test = tast.test;
+    var test = task.test;
     var run = _.isFunction(test.run) ? test.run : test;
     if (_.isFunction(test.before)) {
         test.before(task.desired);

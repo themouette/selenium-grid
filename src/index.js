0e2c96b621e79dd672340c25586880c0c3a7f427
Julien Muetton <themouette@gmail.com>
2013-07-14 02:02:17 +0000

    Expose all.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 module.exports = {
     run: require('./runner'),
-    TestCase: require('./testCase')
+    error: {
+        Browser: require('./error/browser'),
+        Grid: require('./error/grid')
+    },
+    runner: {
+        Grid: require('./runner/grid'),
+        Browser: require('./runner/browser'),
+        Scenario: require('./runner/scenario')
+    },
+    reporter: {
+        Base: require('./reporter/base'),
+        Console: require('./reporter/console')
+    },
+    scenario: {
+        TestCase: require('./scenario/testCase'),
+        Webdriver: require('./scenario/webdriver')
+    }
 };

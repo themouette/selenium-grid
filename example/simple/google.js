07006d192d71da73c4de41e2dfe2de3c4561cbff
Julien Muetton <themouette@gmail.com>
2013-08-20 22:14:40 +0200

    Update simple test.

--- example/simple/google.js
+++ example/simple/google.js
@@ -1,18 +1,27 @@
 
-describe('Google', function () {
-
-    it('should be accessible', function (browser) {
-        browser
-            .start('http://google.fr');
-    });
-
-    it('should have a search input', function(browser) {
-        browser
-            .title(function (title) {
-                assert.equal(title, 'Google');
-            });
-    });
+timeout(10000);
+describe('Google search', function (browser, remote, desired) {
+    var assert = require('chai').assert;
+    browser
+        .get('http://google.fr')
+        .title(function (title) {
+            assert.equal(title, 'Google');
+        })
+        ._takeScreenshot(function (err, image, next) {
+            if (err) {throw err;}
+            saveScreenshot('homepage', image, desired, next);
+        });
 });
 
-it('something', function (browser) {
-});
+function saveScreenshot(id, b64screen, desired, next) {
+    var fs = require('fs');
+    var file = filename(id, desired);
+    fs.writeFile(file, b64screen, 'base64', function (err) {
+        next(err);
+    });
+}
+
+function filename(id, desired) {
+    var path = require('path');
+    return path.resolve(__dirname, '../screenshots', id+'_'+desired.browserName+'.png');
+}
